38a90a2f49425869000e081431016b5e6eb13cc2
Arpita Roy <arpita.roy.workar@gmail.com>
2022-09-12 19:20:55 +0530

    Create the myLeads array and inputEl

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+// Create two variables:
+// myLeads -> should be assigned to an empty array
+// inputEl -> should be assigned to the text input field
+let myLeads = []
+let inputEl = document.getElementById("input-el")
 let inputBtn = document.getElementById("input-btn")
 
 inputBtn.addEventListener("click", function() {
     console.log("Button clicked!")
 })
+
+
