2d840b1188eb0f87f495eae1c575c7feac2d7c38
Arpita Roy <arpita.roy.workar@gmail.com>
2022-09-12 19:48:42 +0530

    Push to the myLeads array

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
-// let myLeads = []
-// const inputEl = document.getElementById("input-el")
-// const inputBtn = document.getElementById("input-btn")
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
 
-// inputBtn.addEventListener("click", function() {
-//     console.log("Button clicked!")
-// })
+// Push the value "www.awesomelead.com" to myArray when the input button is clicked
 
-// let vs. const
-// If possible, use const. If not, use let.
+inputBtn.addEventListener("click", function() {
+    myLeads.push("www.awesomelead.com")
+    console.log(myLeads)
 
-// Which variables below should be changed from let to const?
-
-// The customer wants to order some stuff. Here are the details:
-const basePrice = 520
-const discount = 120
-let shippingCost = 12
-let shippingTime = "5-12 days"
-
-// Whops! Turns out the shipping will be a bit more complex
-shippingCost = 15
-shippingTime = "7-14 days"
-
-// Calculating the full price
-const fullPrice = basePrice - discount + shippingCost
-
-// Finally, notifying the customer
-console.log("Total cost: " + fullPrice + ". It will arrive in " + shippingTime)
+})
