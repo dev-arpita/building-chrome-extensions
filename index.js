84755a791ab49a67b3b3afd8aec13c116a526e0f
Arpita Roy <arpita.roy.workar@gmail.com>
2022-09-12 20:17:27 +0530

    Use a for loop to log out leads

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-let myLeads = []
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
-// Push the value "www.awesomelead.com" to myArray when the input button is clicked
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     console.log(myLeads)
-
 })
+
+
+// Log out the items in the myLeads array using a for loop
+for (let i = 0; i < myLeads.length; i++) {
+    console.log(myLeads[i])
+}
