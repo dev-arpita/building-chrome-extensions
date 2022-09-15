4699c4b84af910c0157214046aac860c24c75783
Arpita Roy <arpita.roy.workar@gmail.com>
2022-09-15 18:50:36 +0530

    Write your first template string

--- index.js
+++ index.js
@@ -1,32 +1,8 @@
-let myLeads = []
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
+// template strings/literals
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-     // Clear out the input field
-      inputEl.value = ""
-     renderLeads()
+const recipient = "James"
 
+// Refactor the email string to use template strings
+const email = "Hey " + recipient + "! How is it going? Cheers Per"
 
-})
-
-function renderLeads() {
-let listItems = ""
-for (let i = 0; i < myLeads.length; i++) {
-    // Wrap the lead in an anchor tag (<a>) inside the <li>
-        // Can you make the link open in a new tab?
-      //   listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
-
-        listItems +=
-        `<li>
-            <a target="_blank" href="${myLeads[i]}">
-            ${myLeads[i]}
-            </a>
-         </li>`
-}
-ulEl.innerHTML = listItems
-}
-
-
+console.log(email)
