f41365efffaef7d3d4d0f2a6651857c274e99746
Arpita Roy <arpita.roy.workar@gmail.com>
2022-09-15 19:07:11 +0530

    Refactor the app to use template string

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-// template strings/literals
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
-const recipient = "James"
-const sender = "Per Harald Borgen"
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    renderLeads()
+})
 
-// Break the email string into multiple lines
-const email = `
-Hey ${recipient}!
-
-How is it going?
-
-Cheers ${sender}
-`
-
-console.log(email)
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        // Refactor the code below to use a template string
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
