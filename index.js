7e85f434f250a00ad8b8c35f44cd8e8d516b83c1
Arpita Roy <arpita.roy.workar@gmail.com>
2022-09-16 23:28:15 +0530

    Create a tab button

--- index.js
+++ index.js
@@ -4,15 +4,21 @@ const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+// 1. Grab the SAVE TAB button and store it in a tabBtn variable
+const tabBtn = document.getElementById("tab-btn")
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     renderLeads()
 }
+const tabs = [
+    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+]
 
-// Refector the function so that it takes a parameter, leads, that it uses
-// instead of the global myLeads variable. Remember to update all invocations
-// of the function as well.
+// 2. Listen for clicks on tabBtn. Log Per's LinkedIn URL to the console
+    tabBtn.addEventListener("click", function() {
+        console.log(tabs[0].url)
+    })
 
 function renderLeads() {
     let listItems = ""
