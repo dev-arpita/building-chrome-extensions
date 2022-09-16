4927ae085bbbcd7124f1fdd6d2d36fc4c97f49b9
Arpita Roy <arpita.roy.workar@gmail.com>
2022-09-16 19:46:26 +0530

    Checking localStorage before rendering

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
-console.log(  Boolean("")   ) // false
-console.log(  Boolean("0")  ) // true
-console.log(  Boolean(100)  ) // true
-console.log(  Boolean(null) ) // false
-console.log(  Boolean([0])  ) // true
-console.log(  Boolean(-0)   ) // false
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+// ["lead1", "lead2"] or null
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+console.log(leadsFromLocalStorage)
+
+// 1. Check if leadsFromLocalStorage is truthy
+// 2. If so, set myLeads to its value and call renderLeads()
+
+if(leadsFromLocalStorage) {
+     myLeads = leadsFromLocalStorage
+     renderLeads()
+}
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    renderLeads()
+})
+
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+
+}
