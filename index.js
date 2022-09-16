b0d4e8595de01beb1007fdeee9727b20951f3803
Arpita Roy <arpita.roy.workar@gmail.com>
2022-09-16 19:21:53 +0530

    Guess the espression - truthy or falsy?

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
-const credits = 12
-
-if (credits) {
-    console.log("Let's play 🎰")
-} else {
-    console.log("Sorry, you have no credits 😭")
-}
-
-// truthy
-// falsy
-
-// false
-// 0
-// ""
-// null -> how you as a developer signalize emptiness
-// undefined -> how JavaScript signalizes emptiness
-// NaN
+console.log(  Boolean("")   ) // false
+console.log(  Boolean("0")  ) // true
+console.log(  Boolean(100)  ) // true
+console.log(  Boolean(null) ) // false
+console.log(  Boolean([0])  ) // true
+console.log(  Boolean(-0)   ) // false
