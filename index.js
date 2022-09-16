8d7a8149272e6be6cb500029f8eac6f5ef43899e
Arpita Roy <arpita.roy.workar@gmail.com>
2022-09-16 17:05:12 +0530

    Storing arrays in local storage

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
-let myLeads = []
+let myLeads = ["www.awesomelead.com"]
+
+// // 1. Turn the myLeads string into an array
+// myLeads = JSON.parse(myLeads)
+// console.log(typeof myLeads)
+
+// // 2. Push a new value to the array
+// myLeads.push("www.epiclead.com")
+// console.log(myLeads)
+
+// 3. Turn the array into a string again
+// 4. Console.log the string using typeof to verify that it's a string
+
+myLeads = JSON.stringify("myLeads")
+console.log(typeof myLeads)
+
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
-
-  // localStorage.setItem("myName", "Arpita Roy")
-
-// let myName = localStorage.getItem("myName")
-//  console.log(myName)
-
-//  localStorage.clear()
-
-
-
-// 1. Save a key-value pair in localStorage
-// 2. Refresh the page. Get the value and log it to the console
-// 3. Clear localStorage
-
-// HINTS:
-// localStorage.setItem(key, value)
-// localStorage.getItem(key)
-// localStorage.clear()
-// PS: both key and value need to be strings
-
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
